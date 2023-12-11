c70dea2850ae416a558d6966dc4f302e4aeeb75b
nchhillar2004 <nchhillar124507@gmail.com>
2023-12-11 17:15:07 +0530

    updated cart

--- client/src/redux/actions/action.js
+++ client/src/redux/actions/action.js
@@ -1,3 +1,17 @@
-export const getProducts =()=>async()=>{
-    
+export const getProducts =()=>async(dispatch)=>{
+    try {
+        const data = await fetch("http://localhost:8080/getproducts/", {
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        });
+
+        const res = await data.json();
+        console.log(res);
+        dispatch({type:"SUCESS_GET_PRODUCTS", payload:res})
+    } catch (error) {
+        dispatch({type:"FAIL_GET_PRODUCTS", payload:error.response})
+        
+    }
 }
